e916b56d864f5d41b9af8c7609a0a469474bdd6b
David Zundel <davidzundel11@gmail.com>
2024-03-18 10:52:49 -0400

    David Zundel: Made the decode() function and changed some stuff in the main method so the variables could work out

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Aiden Blackwood
+# Aiden Blackwood
 def encode(string):
     return_string = ""
     for count in range(len(string)):
@@ -7,26 +7,31 @@ def encode(string):
 
     return return_string
 
-def main():
+
+def decode():
+    return_string = ""
+    for char in encoded_password:
+        return_string += str(int(char)-3)
+    return return_string
+
+
+if __name__ == "__main__":
+    encoded_password = None
     while True:
         print("Menu")
         print("-------------")
         print("1. Encode")
         print("2. Decode")
-        print("3. Quit")
+        print("3. Quit\n")
+        print("Please enter an option: ", end="")
         user_choice = int(input())
-
         if user_choice == 1:
             print("Please enter your password to encode:", end=" ")
             password = input()
             encoded_password = encode(password)
-            print("Your password has been encoded and stored!")
-
+            print("Your password has been encoded and stored!\n")
         elif user_choice == 2:
-            pass
-
+            decoded_password = decode()
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.\n")
         elif user_choice == 3:
             break
-
-if __name__ == "__main__":
-    main()
